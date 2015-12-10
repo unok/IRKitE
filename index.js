dde40def9c2e1006ef19637ffca8af1433ab0d8d
Kazuhiko UNO <kazuhiko.uno@gmail.com>
2015-12-11 02:03:07 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,213 @@
+/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/moment/moment.d.ts" />
+"use strict";
+var remote = require('remote');
+var app = remote.require('app');
+var dialog = remote.require('dialog');
+var http = remote.require('http');
+var ir_messages = [];
+function initialize() {
+    load_messages();
+    home();
+}
+function setting() {
+    $(function () {
+        $('button').removeClass('active');
+        $('span.icon-cog').parent('button').addClass('active');
+        $('.content').hide();
+        $('#setting').show();
+        var ip_selector = $('#ip_address');
+        ip_selector
+            .on('load', function () {
+            update_button_mode_get_client_token($(this));
+        })
+            .on('change', function () {
+            update_button_mode_get_client_token($(this));
+        })
+            .on('keyup', function () {
+            update_button_mode_get_client_token($(this));
+        });
+        ip_selector.val(localStorage.getItem('ip_address') || '');
+        $('#client_key').val(localStorage.getItem('client_key') || '');
+        $('#device_id').val(localStorage.getItem('device_id') || '');
+        update_button_mode_get_client_token(ip_selector);
+        $('#get_client_token_button').on('click', function () {
+            return get_client_token();
+        });
+    });
+    return false;
+}
+function home() {
+    $(function () {
+        $('button').removeClass('active');
+        $('span.icon-home').parent('button').addClass('active');
+        $('.content').hide();
+        $('#home').show();
+        $('#get_message_button').on('click', function () {
+            return get_message();
+        });
+    });
+    update_button_mode_get_message();
+    update_message_list();
+    return false;
+}
+function get_client_token() {
+    var ip_address = $('#ip_address').val();
+    $.ajax({
+        url: "http://" + ip_address + '/keys',
+        type: 'post',
+        dataType: 'json',
+        data: {},
+        headers: {
+            "X-Requested-With": "curl"
+        }
+    }).done(function (response) {
+        console.log('success');
+        console.log(response);
+        localStorage.setItem('ip_address', ip_address);
+        localStorage.setItem('client_token', response.clienttoken);
+        $.ajax({
+            url: "https://api.getirkit.com/1/keys",
+            type: 'post',
+            dataType: 'json',
+            data: {
+                'clienttoken': response.clienttoken
+            },
+            headers: {
+                "X-Requested-With": "curl"
+            }
+        }).done(function (response) {
+            console.log('success');
+            console.log(response);
+            localStorage.setItem('ip_address', ip_address);
+            localStorage.setItem('client_key', response.clientkey);
+            localStorage.setItem('device_id', response.deviceid);
+            $('#client_key').val(response.clientkey);
+            $('#device_id').val(response.deviceid);
+        }).fail(function (response) {
+            console.log('failure');
+            console.log(response);
+        });
+    }).fail(function (response) {
+        console.log('failure');
+        console.log(response);
+    });
+    return false;
+}
+function update_button_mode_get_client_token(column) {
+    var ip_address = column.val();
+    var button = $('#get_client_token_button');
+    if (ip_address.match(/^([0-9]{1,3}\.){3}[0-9]{1,3}$/)) {
+        if (button.prop("disabled") == true) {
+            button.prop("disabled", false);
+            button.removeClass('btn-default');
+            button.addClass('btn-primary');
+        }
+    }
+    else {
+        if (button.prop("disabled") == false) {
+            button.prop("disabled", true);
+            button.removeClass('btn-primary');
+            button.addClass('btn-default');
+        }
+    }
+}
+function update_button_mode_get_message() {
+    var button = $('#get_message_button');
+    if (localStorage.getItem('ip_address') && localStorage.getItem('client_key') && localStorage.getItem('device_id')) {
+        if (button.prop("disabled") == true) {
+            button.prop("disabled", false);
+        }
+    }
+    else {
+        if (button.prop("disabled") == false) {
+            button.prop("disabled", true);
+        }
+    }
+}
+function get_message() {
+    var ip_address = localStorage.getItem('ip_address');
+    $.ajax({
+        url: "http://" + ip_address + '/messages',
+        type: 'get',
+        dataType: 'json',
+        headers: {
+            "X-Requested-With": "curl"
+        }
+    }).done(function (response) {
+        console.log(response);
+        ir_messages[ir_messages.length] = { 'data': response.data, 'freq': response.freq, 'format': response.format };
+        save_messages();
+        update_message_list();
+    }).fail(function (response) {
+        console.log('failure');
+        console.log(response);
+    });
+}
+function post_message(id) {
+    var ip_address = localStorage.getItem('ip_address');
+    $.ajax({
+        url: "http://" + ip_address + '/messages',
+        type: 'post',
+        dataType: 'json',
+        processData: false,
+        data: JSON.stringify({ "message": JSON.stringify(ir_messages[id]) }),
+        headers: {
+            "X-Requested-With": "curl"
+        }
+    }).done(function (response) {
+        console.log(response);
+        var m = moment();
+        ir_messages[ir_messages.length] = { 'data': response.data, 'freq': response.freq, 'format': response.format, 'updated': m.format("YYYY/MM/DD hh:mm:ss") };
+        save_messages();
+    }).fail(function (response) {
+        console.log('failure');
+        console.log(response);
+    });
+}
+function save_messages() {
+    console.log(ir_messages);
+    ir_messages.sort(function (a, b) {
+        if (a.category < b.category) {
+            return -1;
+        }
+        if (a.category > b.category) {
+            return 1;
+        }
+        if (a.action < b.action) {
+            return -1;
+        }
+        if (a.action > b.action) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(ir_messages);
+    localStorage.setItem('messages', JSON.stringify(ir_messages));
+}
+function load_messages() {
+    ir_messages = JSON.parse(localStorage.getItem('messages'));
+}
+function update_message_list() {
+    var table = $('#message_list');
+    var html = '';
+    for (var i = 0; i < ir_messages.length; i++) {
+        var m = ir_messages[i];
+        if (m.data == null) {
+            continue;
+        }
+        html += "\n<tr>\n    <td>\n        <button onclick=\"return post_message(" + i + ");\" class=\"bin btn-positive btn-large\">send</button>\n    </td>\n    <td>\n        <input type=\"text\" class=\"message-action\" id=\"action_" + i + "\" value=\"" + (m.action || '') + "\"/>\n    </td>\n    <td>\n        <input type=\"text\" class=\"message-category\" id=\"category_" + i + "\" value=\"" + (m.category || '') + "\"/>\n    </td>\n    <td>" + (m.updated || '') + "</td>\n</tr>\n";
+    }
+    table.html(html);
+    $('.message-action, .message-category').on('change', function () {
+        var a = $(this).attr('id').split('_');
+        var id = a[1];
+        var m = moment();
+        ir_messages[id].action = $("#action_" + id).val();
+        ir_messages[id].category = $("#category_" + id).val();
+        ir_messages[id].updated = m.format("YYYY/MM/DD hh:mm:ss");
+        save_messages();
+        update_message_list();
+    });
+}
+//# sourceMappingURL=index.js.map
---
+++ main.js
@@ -0,0 +1,24 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+require('crash-reporter').start();
+// メインウィンドウの参照をグローバルに持っておく。
+var mainWindow = null;
+// すべてのウィンドウが閉じられた際の動作
+app.on('window-all-closed', function () {
+    // OS X では、ウィンドウを閉じても一般的にアプリ終了はしないので除外。
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+app.on('ready', function () {
+    // 新規ウィンドウ作成
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.openDevTools();
+    // index.htmlを開く
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    // ウィンドウが閉じられたら、ウィンドウへの参照を破棄する。
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+});
+//# sourceMappingURL=main.js.map
